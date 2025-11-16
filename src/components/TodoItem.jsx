ba9ea5a0a89cad2b90afcd88aaac4ea0588e6cb1
UmarYusuf03 <umaryusuf0307@gmail.com>
2025-11-16 22:15:56 +0530

    completed TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -38,7 +38,24 @@ function TodoItem({ todo }) {
         onChange={(e) => setTodoMsg(e.target.value)}
         readOnly={!isTodoEditable}
       />
-      <button className="">{isTodoEditable ? "Save" : "Edit"}</button>
+      <button 
+      className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100
+      shrink-0 disabled:opacity-50"
+      onClick={() =>{
+        if(todo.completed) return
+        if(isTodoEditable){
+          editTodo()
+        }else setIsTodoEditable((prev) => !prev)
+      }}
+      disabled={todo.completed}
+      >
+        {isTodoEditable ? "Save" : "Edit"}</button>
+
+      <button
+      className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+      onClick={() => deleteTodo(todo.id)}
+      >
+        Delete</button>  
     </div>
   );
 }
